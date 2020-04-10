be96d506b4344790e2c2ef4dff6b60ef16da6f9e
Heather Stoffels <Umdstarlet@gmail.com>
2020-04-10 15:28:59 -0500

    created all new files for project

---
+++ client.js
@@ -0,0 +1,23 @@
+$(document).ready(inReady);
+
+function inReady() {
+  console.log("in ready");
+}
+// create an array of employee objects
+
+// create 5 input fields
+//employee.firstName
+//employee.lastName
+//employee.idNumber
+//employee.jobTitle
+//employee.annualSalary
+
+//create button to take in all 5 inputs
+
+//make sure none of the inputs are blank
+//if unput(s) are black make .alert
+
+// add salaries to report monthly cost
+// divide annual salary by 12
+//if monthly salary is over 20,000, add red background to
+// total monthly cost
